fe77536057a6c903b5961abd86b5a779b4c287b6
Bhagyashree <bhag41@gmail.com>
2019-11-22 14:34:12 -0800

    added the correct Edit profile component name. there was typo before

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -9,7 +9,7 @@ import SelectListGroup from "../common/SelectListGroup";
 import { createProfile, getCurrentProfile } from "../../actions/profileActions";
 import isEmpty from "../../validation/is-empty";
 
-class CreateProfile extends Component {
+class EditProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
