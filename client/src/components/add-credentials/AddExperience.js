9d767618a6844aa3f50530ed6fdabde963bca3b2
Bhagyashree <bhag41@gmail.com>
2019-12-03 13:22:15 -0800

    started working on add credentials component

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom';
+import { TextFieldGroup } from '../common/TextFieldGroup';
+import { TextAreaFieldGroup } from '../common/TextAreaFieldGroup';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+
+ class AddExperience extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default AddExperience;
